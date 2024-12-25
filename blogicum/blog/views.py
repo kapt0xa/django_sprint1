a3d7e00051535d25e991787f09798149fdf8b2f7
kapt0xa <kuzmich.ml@gmail.com>
2024-12-25 03:57:44 +0300

    view functions update

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from pathlib import Path
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def post_detail(request):
+    return render(request, 'blog/post_detail.html')
+
+def category_posts(request):
+    return render(request, 'blog/category_posts.html')
