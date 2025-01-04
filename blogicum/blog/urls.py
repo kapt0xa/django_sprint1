83d1be333ef4f774c7b63a53c6711ba863daec46
kapt0xa <kuzmich.ml@gmail.com>
2025-01-04 04:56:43 +0300

    the "black" module formated my files

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from blog import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('category_posts/<str:category>', views.category_posts, name='category_posts'),
-    path('post_detail/<int:id>/', views.post_detail, name='post_detail'),
+    path("", views.index, name="index"),
+    path("category_posts/<str:category>", views.category_posts, name="category_posts"),
+    path("post_detail/<int:id>/", views.post_detail, name="post_detail"),
 ]
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from pages import views
 
-app_name = 'pages'
+app_name = "pages"
 
 urlpatterns = [
-    path('rules/', views.rules, name='rules'),
-    path('about/', views.about, name='about'),
+    path("rules/", views.rules, name="rules"),
+    path("about/", views.about, name="about"),
 ]
