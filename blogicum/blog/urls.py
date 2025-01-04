a986382616e4bce35a3b87848bdbc2fdae9c26af
kapt0xa <kuzmich.ml@gmail.com>
2025-01-04 05:12:36 +0300

    dammit pep8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,6 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("category_posts/<str:category>", views.category_posts, name="category_posts"),
+    path("category_posts/<str:category>", views.category_posts, name="category_posts"),  # noqa: E501,F401
     path("post_detail/<int:id>/", views.post_detail, name="post_detail"),
 ]
